2a36e003eca3a7d265e4c882d450e17c19e76410
mathewhoyerstokes <57637121+mathewhoyerstokes@users.noreply.github.com>
2022-03-08 07:40:29 +0100

    [FIX] fix map on loading, show map on deployment (due to import of package)

--- src/Map.js
+++ src/Map.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
+import mapbox from 'mapbox-gl';
 
 class Map extends Component {
   componentDidMount() {
     // use the const app to pass stuff into the map
     const app = this.props.app
-    mapboxgl.accessToken = 'pk.eyJ1IjoibXN0b2tlczMxMDgiLCJhIjoiY2wwZnU1d2pzMHY0YTNka3htaTU5bHRhYyJ9.3DjvUhpVMWKyY8afdFYgFQ';
+    mapbox.accessToken = 'pk.eyJ1IjoibXN0b2tlczMxMDgiLCJhIjoiY2wwZnU1d2pzMHY0YTNka3htaTU5bHRhYyJ9.3DjvUhpVMWKyY8afdFYgFQ';
 
-    const map = new mapboxgl.Map({
+    const map = new mapbox.Map({
       container: 'map',
       style: app.state.style,
       center: [
@@ -17,7 +17,7 @@ class Map extends Component {
       zoom: 12
     });
 
-    map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(new mapbox.NavigationControl());
 
     this.props.app.setState({
       map: map
